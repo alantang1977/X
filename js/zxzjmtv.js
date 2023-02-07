2d604f6cf1a4b1f7c3b218537577a9df56dd5ba4
超人哥哥 <107459091+alantang1977@users.noreply.github.com>
2023-02-07 15:14:38 +0800

    Update zxzjmtv.js

--- js/zxzjmtv.js
+++ js/zxzjmtv.js
@@ -1,33 +1,20 @@
-// 网址发布页 www.qiyoudy.info
-muban.首图2.二级.title = 'h1&&Text;.data--span:eq(0)&&Text';
-muban.首图2.二级.desc = '.data.hidden-sm&&Text;;;p.line1--span&&Text;.data--span:eq(1)&&Text';
-muban.首图2.二级.content = '.desc--span&&Text';
-muban.首图2.二级.tabs = '.nav-tabs a';
 var rule = {
-    title:'奇优影院',
-    模板:'首图2',
-    host:'http://1e6e.com',
-    // url:'/list/fyclass_fypage.html',
-    url:'/list/fyclass_fypage.html?order=fyfilter',
-    filterable:1,//是否启用分类筛选,
-    filter_url:'{{fl.by}}',
-    filter:{
-        "1":[{"key":"by","name":"排序","value":[{"n":"按时间","v":"time"},{"n":"按人气","v":"hit"}]}],
-        "2":[{"key":"by","name":"排序","value":[{"n":"按时间","v":"time"},{"n":"按人气","v":"hit"}]}],
-        "3":[{"key":"by","name":"排序","value":[{"n":"按时间","v":"time"},{"n":"按人气","v":"hit"}]}],
-        "4":[{"key":"by","name":"排序","value":[{"n":"按时间","v":"time"},{"n":"按人气","v":"hit"}]}],
-        "6":[{"key":"by","name":"排序","value":[{"n":"按时间","v":"time"},{"n":"按人气","v":"hit"}]}]
+    title:'追剧迷',
+    模板:'vfed',
+    host:'https://www.zhuijumi.cc',
+    url:'/videotype/fyclass-fypage.html',
+    searchUrl:'/vodsearch/**-fypage.html',
+    class_parse: '.fed-part-tips li;a&&Text;a&&href;.*/(.*?).html',
+    cate_exclude:'更多|申请',
+	lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
+    二级: {
+        "title": "h1&&Text;.fed-col-xs6--span:eq(0)&&Text",
+        "img": ".fed-list-info&&a&&data-original",
+        "desc": ".fed-col-xs12.fed-part-eone:eq(3)&&Text;;;.fed-col-xs12.fed-part-eone--span:eq(0)&&Text;.fed-col-xs12.fed-part-eone--span:eq(1)&&Text",
+        "content": ".fed-conv-text:eq(0)&&Text",
+        "tabs": "ul.fed-padding&&li",
+        "lists": ".fed-tabs-btm:eq(#id) li"
     },
-    filter_def:{
-        1:{by:'time'},
-        2:{by:'time'},
-        3:{by:'time'},
-        4:{by:'time'},
-        6:{by:'time'}
-    },
-    class_parse: '.stui-header__menu li:gt(0):lt(5);a&&Text;a&&href;.*/(.*?).html',
-    // searchUrl:'/search.php;**',
-    searchUrl:'/search.php#searchword=**;post',
-    // 搜索:'js:let url=input.split(";")[0];let d=[];let body={searchword:input.split(";")[1]};body="searchword="+input.split(";")[1];fetch_params.body=body;let html=post(url,fetch_params);let pdfa=jsp.pdfa;let pdfh=jsp.pdfh;let pd=jsp.pd;let lists=pdfa(html,"ul.stui-vodlist__media&&li");lists.forEach(function(it){d.push({title:pdfh(it,".title&&Text"),url:pd(it,"a&&href"),desc:pdfh(html,".pic-text&&Text"),pic_url:pd(html,".lazyload&&data-original")})});setResult(d);',
-    //搜索:'li.stui-vodlist__item;a&&title;a&&data-original;.pic-text&&Text;a&&href'
+    搜索: '.fed-list-deta;h1&&Text;.fed-lazy&&data-original;.fed-list-remarks&&Text;a&&href;.fed-col-xs12.fed-part-eone:eq(2)&&Text',
+
 }
