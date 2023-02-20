c10eaa23ef8124ff2cd077369e6c03e842145bef
超人哥哥 <107459091+alantang1977@users.noreply.github.com>
2023-02-20 15:44:25 +0800

    Update 新视觉影视.js

--- js/新视觉影视.js
+++ js/新视觉影视.js
@@ -1,6 +1,6 @@
 var rule={
 	title:'新视觉影视',
-	host:'https://www.6080dy2.com',
+	host:'https://www.2wyy.com/',
 	// url:'/vodshow/fyclass--------fypage---.html',
 	url:'/vodshow/fyfilter.html',
     filterable:1,//是否启用分类筛选,
