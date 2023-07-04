ca12cd251c01ecb5e011e1ffe356a425cb5d444a
alantang <107459091+alantang1977@users.noreply.github.com>
2023-07-04 16:13:54 +0800

    Update 爱壹帆.js

--- js/爱壹帆.js
+++ js/爱壹帆.js
@@ -5,7 +5,7 @@ var rule = {
     模板:'海螺3',
     host:'https://www.iyf1.tv',
     // url:'/vodshow/fyclass--------fypage---.html',
-    url:'/vodshow/fyfilter.html',
+   url:'/index.php/vod/show/id/fyfilter.html',
     filterable:1,//是否启用分类筛选,
     filter_url:'{{fl.cateId}}-{{fl.area}}-{{fl.by}}-{{fl.class}}-{{fl.lang}}-{{fl.letter}}---fypage---{{fl.year}}',
     filter: {
