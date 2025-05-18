275dc64929c1e17a94f73ffca5b15b3c36ca72ab
alantang <alanchao@163.com>
2025-05-19 00:00:22 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,116 @@
+# 配置文件，包含直播源URL、黑名单URL、公告信息、EPG URL、测速超时时间和线程池最大工作线程数
+
+# 优先使用的IP版本，这里设置为ipv6
+ip_version_priority = "ipv6"
+
+# 直播源URL列表
+source_urls = [
+    "http://api.mytv666.top/lives/free.php?type=txt",
+    "http://zhibo.feylen.top/fltv/js/ku9live.php?tpye=fl.txt",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/Alan-Alana/IPTV/refs/heads/main/channl.txt",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/peterHchina/iptv/refs/heads/main/IPTV-V4.m3u",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/peterHchina/iptv/refs/heads/main/IPTV-V6.m3u",
+    "http://92.112.21.169:30000/mytv.m3u",
+    "http://lisha521.dynv6.net.fh4u.org/tv.txt",
+    "https://web.banye.tech:7777/tv/hlj.txt",
+    "https://iptv.catvod.com/tv.m3u",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/hostemail/cdn/main/live/tv.txt",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/alantang1977/JunTV/refs/heads/main/output/result.m3u",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/ssili126/tv/main/itvlist.m3u",
+    "https://live.zbds.top/tv/iptv4.txt",
+    "https://gitee.com/xxy002/zhiboyuan/raw/master/dsy",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/big-mouth-cn/tv/main/iptv-ok.m3u",
+    "https://codeberg.org/alfredisme/mytvsources/raw/branch/main/mylist-ipv6.m3u",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/lalifeier/IPTV/main/m3u/IPTV.m3u",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/wwb521/live/main/tv.m3u",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/asdjkl6/tv/tv/.m3u/整套直播源/测试/整套直播源/l.txt",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/asdjkl6/tv/tv/.m3u/整套直播源/测试/整套直播源/kk.txt",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/suxuang/myIPTV/main/ipv6.m3u",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/Guovin/iptv-api/gd/output/result.m3u",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/yuanzl77/IPTV/main/live.m3u",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/YanG-1989/m3u/main/Gather.m3u",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/YueChan/Live/refs/heads/main/APTV.m3u",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/Kimentanm/aptv/master/m3u/iptv.m3u",
+    "https://live.zbds.top/tv/iptv6.txt",
+    "http://xhztv.top/new.txt",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/Guovin/TV/gd/output/result.txt",
+    "http://home.jundie.top:81/Cat/tv/live.txt",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/vbskycn/iptv/master/tv/hd.txt",
+    "https://live.fanmingming.cn/tv/m3u/ipv6.m3u",
+    "https://live.zhoujie218.top/tv/iptv6.txt",
+    "https://cdn.jsdelivr.net/gh/YueChan/live@main/IPTV.m3u",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/cymz6/AutoIPTV-Hotel/main/lives.txt",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/PizazzGY/TVBox_warehouse/main/live.txt",
+    "https://fm1077.serv00.net/SmartTV.m3u",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/joevess/IPTV/main/home.m3u8",
+    "https://tv.iill.top/m3u/Gather",
+    "https://m3u.ibert.me/txt/o_cn.txt",
+    "https://m3u.ibert.me/txt/j_iptv.txt",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/xzw832/cmys/main/S_CCTV.txt",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/xzw832/cmys/main/S_weishi.txt",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/BurningC4/Chinese-IPTV/master/TV-IPV4.m3u",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/joevess/IPTV/main/m3u/iptv.m3u",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/Ftindy/IPTV-URL/main/IPV6.m3u",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/YanG-1989/m3u/main/Gather.m3u",
+    "https://iptv.b2og.com/txt/fmml_ipv6.txt",
+    "http://xhztv.top/zbc.txt",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output_simple.txt",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/SPX372928/MyIPTV/master/黑龙江PLTV移动CDN版.txt",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/qingwen07/awesome-iptv/main/tvbox_live_all.txt",
+    "https://gh.tryxd.cn/https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.m3u",
+    "https://live.zhoujie218.top/tv/iptv4.txt"
+]
+
+# 直播源黑名单URL列表，去除了重复项
+url_blacklist = [
+    "epg.pw/stream/",
+    "103.40.13.71:12390",
+    "[2409:8087:1a01:df::4077]/PLTV/",
+    "http://[2409:8087:1a01:df::7005]:80/ottrrs.hl.chinamobile.com/PLTV/88888888/224/3221226419/index.m3u8",
+    "http://[2409:8087:5e00:24::1e]:6060/000000001000/1000000006000233001/1.m3u8",
+    "8.210.140.75:68",
+    "154.12.50.54",
+    "yinhe.live_hls.zte.com",
+    "8.137.59.151",
+    "[2409:8087:7000:20:1000::22]:6060",
+    "histar.zapi.us.kg",
+    "www.tfiplaytv.vip",
+    "dp.sxtv.top",
+    "111.230.30.193",
+    "148.135.93.213:81",
+    "live.goodiptv.club",
+    "iptv.luas.edu.cn",
+    "[2409:8087:2001:20:2800:0:df6e:eb22]:80",
+    "[2409:8087:2001:20:2800:0:df6e:eb23]:80",
+    "[2409:8087:2001:20:2800:0:df6e:eb1d]/ott.mobaibox.com/",
+    "[2409:8087:2001:20:2800:0:df6e:eb1d]:80",
+    "[2409:8087:2001:20:2800:0:df6e:eb24]",
+    "2409:8087:2001:20:2800:0:df6e:eb25]:80",
+    "stream1.freetv.fun",
+    "chinamobile",
+    "gaoma",
+    "[2409:8087:2001:20:2800:0:df6e:eb27]"
+]
+
+# 公告信息
+announcements = [
+    {
+        "channel": "更新日期",
+        "entries": [
+            {
+                "name": None,
+                "url": "https://gh.tryxd.cn/https://raw.githubusercontent.com/alantang1977/X/main/Pictures/Robot.mp4",
+                "logo": "https://gh.tryxd.cn/https://raw.githubusercontent.com/alantang1977/X/main/Pictures/TangImage320.png"
+            }
+        ]
+    }
+]
+
+# EPG（电子节目指南）URL列表
+epg_urls = [
+    "https://epg.v1.mk/fy.xml",
+    "http://epg.51zmt.top:8000/e.xml",
+    "https://epg.pw/xmltv/epg_CN.xml",
+    "https://epg.pw/xmltv/epg_HK.xml",
+    "https://epg.pw/xmltv/epg_TW.xml"
+]
---
+++ main.py
@@ -0,0 +1,279 @@
+import re
+import requests
+import logging
+from collections import OrderedDict
+from datetime import datetime
+import config
+import os
+import difflib
+
+# 日志记录。
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
+                    handlers=[logging.FileHandler("function.log", "w", encoding="utf-8"), logging.StreamHandler()])
+
+# 确保 output 文件夹存在
+output_folder = "output"
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+def parse_template(template_file):
+    # 解析模板文件，提取频道分类和频道名称。
+    template_channels = OrderedDict()
+    current_category = None
+
+    with open(template_file, "r", encoding="utf-8") as f:
+        for line in f:
+            line = line.strip()
+            if line and not line.startswith("#"):
+                if "#genre#" in line:
+                    # 提取当前类别
+                    current_category = line.split(",")[0].strip()
+                    template_channels[current_category] = []
+                elif current_category:
+                    # 提取频道名称并加入当前类别中
+                    channel_name = line.split(",")[0].strip()
+                    template_channels[current_category].append(channel_name)
+
+    return template_channels
+
+# 数据清洗函数
+def clean_channel_name(channel_name):
+    cleaned_name = re.sub(r'[$「」-]', '', channel_name)  # 去掉中括号、«», 和'-'字符
+    cleaned_name = re.sub(r'\s+', '', cleaned_name)  # 去掉所有空白字符
+    cleaned_name = re.sub(r'(\D*)(\d+)', lambda m: m.group(1) + str(int(m.group(2))), cleaned_name)  # 将数字前面的部分保留，数字转换为整数
+    return cleaned_name.upper()  # 转换为大写
+
+def fetch_channels(url):
+    # 从指定URL抓取频道列表。
+    channels = OrderedDict()
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        response.encoding = 'utf-8'
+        lines = response.text.split("\n")
+        current_category = None
+        is_m3u = any(line.startswith("#EXTINF") for line in lines[:15])
+        source_type = "m3u" if is_m3u else "txt"
+        logging.info(f"url: {url} 成功，判断为{source_type}格式")
+
+        if is_m3u:
+            channels.update(parse_m3u_lines(lines))
+        else:
+            channels.update(parse_txt_lines(lines))
+
+        if channels:
+            categories = ", ".join(channels.keys())
+            logging.info(f"url: {url} 成功，包含频道分类: {categories}")
+    except requests.RequestException as e:
+        logging.error(f"url: {url} 失败❌, Error: {e}")
+
+    return channels
+
+def parse_m3u_lines(lines):
+    # 解析M3U格式的频道列表行。
+    channels = OrderedDict()
+    current_category = None
+
+    for line in lines:
+        line = line.strip()
+        if line.startswith("#EXTINF"):
+            match = re.search(r'group-title="(.*?)",(.*)', line)
+            if match:
+                current_category = match.group(1).strip()
+                channel_name = match.group(2).strip()
+                if channel_name and channel_name.startswith("CCTV"):  # 判断频道名称是否存在且以CCTV开头
+                    channel_name = clean_channel_name(channel_name)  # 频道名称数据清洗
+
+                if current_category not in channels:
+                    channels[current_category] = []
+        elif line and not line.startswith("#"):
+            channel_url = line.strip()
+            if current_category and channel_name:
+                # 添加频道信息到当前类别中
+                channels[current_category].append((channel_name, channel_url))
+
+    return channels
+
+def parse_txt_lines(lines):
+    # 解析TXT格式的频道列表行。
+    channels = OrderedDict()
+    current_category = None
+
+    for line in lines:
+        line = line.strip()
+        if "#genre#" in line:
+            # 提取当前类别
+            current_category = line.split(",")[0].strip()
+            channels[current_category] = []
+        elif current_category:
+            match = re.match(r"^(.*?),(.*?)$", line)
+            if match:
+                channel_name = match.group(1).strip()
+                if channel_name and channel_name.startswith("CCTV"):  # 判断频道名称是否存在且以CCTV开头
+                    channel_name = clean_channel_name(channel_name)  # 频道名称数据清洗
+                # 提取频道URL，并分割成多个部分
+                channel_urls = match.group(2).strip().split('#')
+
+                # 存储每个分割出的URL
+                for channel_url in channel_urls:
+                    channel_url = channel_url.strip()  # 去掉前后空白
+                    channels[current_category].append((channel_name, channel_url))
+            elif line:
+                channels[current_category].append((line, ''))
+
+    return channels
+
+def find_similar_name(target_name, name_list):
+    # 查找最相似的名称
+    matches = difflib.get_close_matches(target_name, name_list, n=1, cutoff=0.6)
+    return matches[0] if matches else None
+
+def match_channels(template_channels, all_channels):
+    # 匹配模板中的频道与抓取到的频道。
+    matched_channels = OrderedDict()
+
+    all_online_channel_names = []
+    for online_category, online_channel_list in all_channels.items():
+        for online_channel_name, _ in online_channel_list:
+            all_online_channel_names.append(online_channel_name)
+
+    for category, channel_list in template_channels.items():
+        matched_channels[category] = OrderedDict()
+        for channel_name in channel_list:
+            similar_name = find_similar_name(channel_name, all_online_channel_names)
+            if similar_name:
+                for online_category, online_channel_list in all_channels.items():
+                    for online_channel_name, online_channel_url in online_channel_list:
+                        if online_channel_name == similar_name:
+                            # 匹配成功的频道信息加入结果中
+                            matched_channels[category].setdefault(channel_name, []).append(online_channel_url)
+
+    return matched_channels
+
+def filter_source_urls(template_file):
+    # 过滤源URL，获取匹配后的频道信息。
+    template_channels = parse_template(template_file)
+    source_urls = config.source_urls
+
+    all_channels = OrderedDict()
+    for url in source_urls:
+        fetched_channels = fetch_channels(url)
+        merge_channels(all_channels, fetched_channels)
+
+    matched_channels = match_channels(template_channels, all_channels)
+
+    return matched_channels, template_channels
+
+def merge_channels(target, source):
+    # 合并两个频道字典。
+    for category, channel_list in source.items():
+        if category in target:
+            target[category].extend(channel_list)
+        else:
+            target[category] = channel_list
+
+def is_ipv6(url):
+    # 判断URL是否为IPv6地址。
+    return re.match(r'^http:\/\/\[[0-9a-fA-F:]+\]', url) is not None
+
+def updateChannelUrlsM3U(channels, template_channels):
+    # 更新频道URL到M3U和TXT文件中。
+    written_urls_ipv4 = set()
+    written_urls_ipv6 = set()
+
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    for group in config.announcements:
+        for announcement in group['entries']:
+            if announcement['name'] is None:
+                announcement['name'] = current_date
+
+    ipv4_m3u_path = os.path.join(output_folder, "live_ipv4.m3u")
+    ipv4_txt_path = os.path.join(output_folder, "live_ipv4.txt")
+    ipv6_m3u_path = os.path.join(output_folder, "live_ipv6.m3u")
+    ipv6_txt_path = os.path.join(output_folder, "live_ipv6.txt")
+
+    with open(ipv4_m3u_path, "w", encoding="utf-8") as f_m3u_ipv4, \
+            open(ipv4_txt_path, "w", encoding="utf-8") as f_txt_ipv4, \
+            open(ipv6_m3u_path, "w", encoding="utf-8") as f_m3u_ipv6, \
+            open(ipv6_txt_path, "w", encoding="utf-8") as f_txt_ipv6:
+
+        f_m3u_ipv4.write(f"""#EXTM3U x-tvg-url={",".join(f'"{epg_url}"' for epg_url in config.epg_urls)}\n""")
+        f_m3u_ipv6.write(f"""#EXTM3U x-tvg-url={",".join(f'"{epg_url}"' for epg_url in config.epg_urls)}\n""")
+
+        for group in config.announcements:
+            f_txt_ipv4.write(f"{group['channel']},#genre#\n")
+            f_txt_ipv6.write(f"{group['channel']},#genre#\n")
+            for announcement in group['entries']:
+                url = announcement['url']
+                if is_ipv6(url):
+                    if url not in written_urls_ipv6:
+                        written_urls_ipv6.add(url)
+                        f_m3u_ipv6.write(f"""#EXTINF:-1 tvg-id="1" tvg-name="{announcement['name']}" tvg-logo="{announcement['logo']}" group-title="{group['channel']}",{announcement['name']}\n""")
+                        f_m3u_ipv6.write(f"{url}\n")
+                        f_txt_ipv6.write(f"{announcement['name']},{url}\n")
+                else:
+                    if url not in written_urls_ipv4:
+                        written_urls_ipv4.add(url)
+                        f_m3u_ipv4.write(f"""#EXTINF:-1 tvg-id="1" tvg-name="{announcement['name']}" tvg-logo="{announcement['logo']}" group-title="{group['channel']}",{announcement['name']}\n""")
+                        f_m3u_ipv4.write(f"{url}\n")
+                        f_txt_ipv4.write(f"{announcement['name']},{url}\n")
+
+        for category, channel_list in template_channels.items():
+            f_txt_ipv4.write(f"{category},#genre#\n")
+            f_txt_ipv6.write(f"{category},#genre#\n")
+            if category in channels:
+                for channel_name in channel_list:
+                    if channel_name in channels[category]:
+                        sorted_urls_ipv4 = []
+                        sorted_urls_ipv6 = []
+                        for url in channels[category][channel_name]:
+                            if is_ipv6(url):
+                                if url not in written_urls_ipv6:
+                                    sorted_urls_ipv6.append(url)
+                                    written_urls_ipv6.add(url)
+                            else:
+                                if url not in written_urls_ipv4:
+                                    sorted_urls_ipv4.append(url)
+                                    written_urls_ipv4.add(url)
+
+                        total_urls_ipv4 = len(sorted_urls_ipv4)
+                        total_urls_ipv6 = len(sorted_urls_ipv6)
+
+                        for index, url in enumerate(sorted_urls_ipv4, start=1):
+                            new_url = add_url_suffix(url, index, total_urls_ipv4, "IPV4")
+                            write_to_files(f_m3u_ipv4, f_txt_ipv4, category, channel_name, index, new_url)
+
+                        for index, url in enumerate(sorted_urls_ipv6, start=1):
+                            new_url = add_url_suffix(url, index, total_urls_ipv6, "IPV6")
+                            write_to_files(f_m3u_ipv6, f_txt_ipv6, category, channel_name, index, new_url)
+
+        f_txt_ipv4.write("\n")
+        f_txt_ipv6.write("\n")
+
+def sort_and_filter_urls(urls, written_urls):
+    # 排序和过滤URL。
+    filtered_urls = [
+        url for url in sorted(urls, key=lambda u: not is_ipv6(u) if config.ip_version_priority == "ipv6" else is_ipv6(u))
+        if url and url not in written_urls and not any(blacklist in url for blacklist in config.url_blacklist)
+    ]
+    written_urls.update(filtered_urls)
+    return filtered_urls
+
+def add_url_suffix(url, index, total_urls, ip_version):
+    # 添加URL后缀。
+    suffix = f"${ip_version}" if total_urls == 1 else f"${ip_version}•线路{index}"
+    base_url = url.split('$', 1)[0] if '$' in url else url
+    return f"{base_url}{suffix}"
+
+def write_to_files(f_m3u, f_txt, category, channel_name, index, new_url):
+    # 写入M3U和TXT文件。
+    logo_url = f"https://gitee.com/IIII-9306/PAV/raw/master/logos/{channel_name}.png"
+    f_m3u.write(f"#EXTINF:-1 tvg-id=\"{index}\" tvg-name=\"{channel_name}\" tvg-logo=\"{logo_url}\" group-title=\"{category}\",{channel_name}\n")
+    f_m3u.write(new_url + "\n")
+    f_txt.write(f"{channel_name},{new_url}\n")
+
+if __name__ == "__main__":
+    template_file = "demo.txt"
+    channels, template_channels = filter_source_urls(template_file)
+    updateChannelUrlsM3U(channels, template_channels)
