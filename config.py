2a3ec486e8b716d8be77009e4296666dcfc808aa
alantang <alanchao@163.com>
2025-05-21 22:01:55 +0800

    Update config.py

--- config.py
+++ config.py
@@ -115,7 +115,7 @@ announcements = [
             {
                 "name": None,
                 "url": "https://gh.tryxd.cn/https://raw.githubusercontent.com/alantang1977/X/main/Pictures/Robot.mp4",
-                "logo": "https://gh.tryxd.cn/https://raw.githubusercontent.com/alantang1977/X/main/Pictures/HDgreen240.png"
+                "logo": "https://gh.tryxd.cn/https://raw.githubusercontent.com/alantang1977/X/main/Pictures/chao-assets.png"
             }
         ]
     }
