a5fbd4b16ea5e1ecbb5331765bff3cf88f7698c8
alantang <alanchao@163.com>
2025-07-13 11:44:22 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ ip_version_priority = "ipv4"
 
 # 直播源URL列表
 source_urls = [
+    "http://kds.shanxixr.com/migu01.txt",
     "http://is.is-great.org/i/0885557.txt",
     "https://115.190.105.236/vip/qwt.m3u",
     "https://115.190.105.236/vip/vip.m3u",
