3bb15a8db495d46d3dd5e1e649a6f6b4fc9dfc54
alantang <alanchao@163.com>
2025-10-22 13:36:42 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,9 @@ ip_version_priority = "ipv4"
 # 直播源URL列表
 source_urls = [
     "https://gitee.com/jin-xueling/lingl/raw/master/hu.txt",
+    "https://gh.catmak.name/https://raw.githubusercontent.com/develop202/migu_video/main/interface.txt",
     "https://raw.githubusercontent.com/mursor1985/LIVE/refs/heads/main/iptv.m3u",
-    "https://gitee.com/junchao321/migu_video/raw/main/interface.txt",
+    "https://fy.188766.xyz/?url=https%3A%2F%2Flive.tangshop.top%3A35455&mishitong=true&mima=mianfeidehaimaiqian&json=true",
     "https://d.kstore.dev/download/15114/TVSolo.txt",
     "https://raw.githubusercontent.com/iptv-org/iptv/gh-pages/countries/cn.m3u",
     "https://raw.githubusercontent.com/iptv-org/iptv/master/streams/cn.m3u",
