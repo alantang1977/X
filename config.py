db3f546b00362545a7d0656ac7b8954d86ba668a
alantang <alanchao@163.com>
2025-10-22 13:40:25 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ ip_version_priority = "ipv4"
 
 # 直播源URL列表
 source_urls = [
+    "https://gh.catmak.name/https://raw.githubusercontent.com/alantang1977/jtv/refs/heads/main/网络收集.txt",
     "https://gitee.com/jin-xueling/lingl/raw/master/hu.txt",
     "https://gh.catmak.name/https://raw.githubusercontent.com/develop202/migu_video/main/interface.txt",
     "https://raw.githubusercontent.com/mursor1985/LIVE/refs/heads/main/iptv.m3u",
