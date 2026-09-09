b91bb96c18723808c33d36a9d9edf5939d7bf3b5
alantang <alanchao@163.com>
2026-09-09 15:09:45 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ ip_version_priority = "ipv4"
 
 # 直播源URL列表
 source_urls = [
+    "https://live.445569.xyz/live.m3u",
     "https://raw.githubusercontent.com/aiyakuaile/easy_tv_live/refs/heads/main/temp",
     "https://raw.githubusercontent.com/alantang1977/jtv/refs/heads/main/网络收集.txt",
     "https://gitee.com/jin-xueling/lingl/raw/master/hu.txt",
